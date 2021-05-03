1f9fab2a7a30db148556e4cb743436082e47d48e
Stevtm <steventmaddison@outlook.com>
2021-05-03 15:29:42 -0400

    add first query to database

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const mysql = require("mysql2");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -6,6 +7,21 @@ const app = express();
 // express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+// connect to database
+const db = mysql.createConnection(
+	{
+		host: "localhost",
+		user: "root",
+		password: "U#wwGWpy5x!L6Y",
+		database: "election",
+	},
+	console.log("Connected to the election database.")
+);
+
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+	console.log(rows);
+});
 
 // default response for any other request (Not Found)
 app.use((req, res) => {
