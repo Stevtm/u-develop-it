8220dbd64317c038f345837e111033f46da08de0
Stevtm <steventmaddison@outlook.com>
2021-05-03 15:50:10 -0400

    add query to create a candidate

--- server.js
+++ server.js
@@ -32,10 +32,19 @@ db.query(`SELECT * FROM candidates WHERE id = 10`, (err, rows) => {
 	console.log(rows);
 });
 
-// create a candidate
-
 // Delete a candidate
 db.query(`DELETE FROM candidates WHERE id = ?`, 1, (err, result) => {
+	if (err) {
+		console.log(err);
+	}
+	console.log(result);
+});
+
+// create a candidate
+const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected) VALUES (?, ?, ?, ?)`;
+const params = [1, "Ronald", "Firbank", 1];
+
+db.query(sql, params, (err, result) => {
 	if (err) {
 		console.log(err);
 	}
